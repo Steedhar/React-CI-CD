096f6cffff61a1dde4d2441206bfe1cd75d20b9e
steedhar <steedharravi@gmail.com>
2025-10-04 17:32:00 +0530

    test file added

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import TestCom from './test';
 
 function App() {
   return (
     <div className="App">
     <span>hjdhfksdjghkdhgkdhgkldfjgkldfjgldfkgjdfklgj react</span>
     <div>jskefhsjkfhsdkjhfsdjkfhksdjhfsdkjfhekyuiwefhkewjfkwjefiwehkfjhwekjfhewkj</div>
+    <TestCom/>
     </div>
   );
 }
---
+++ my-react-app/src/test.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function TestCom() {
+  return (
+    <div className="App">
+
+    <div>this is test file of react</div>
+    </div>
+  );
+}
+
+export default TestCom;
