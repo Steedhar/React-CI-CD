47d206b10b8210974f6ecb2d009a1d33b852167f
steedhar <steedharravi@gmail.com>
2025-10-04 15:44:48 +0530

    Add .dockerignore file and update greeting in App component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <span>hi this our first cicd in react</span>
+    <span>hi this our first cicd in  node and react</span>
     </div>
   );
 }
