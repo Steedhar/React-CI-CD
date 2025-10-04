6264f3fa676fe741ce47db925020ab05a38e40e8
steedhar <steedharravi@gmail.com>
2025-10-04 17:38:25 +0530

    Update Jenkinsfile with correct EC2 host and fix typo in App component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
     <span>hjdhfksdjghkdhgkdhgkldfjgkldfjgldfkgjdfklgj react</span>
-    <div>jskefhsjkfhsdkjhfsdjkfhksdjhfsdkjfhekyuiwefhkewjfkwjefiwehkfjhwekjfhewkj</div>
+    <div>jskefhssdjkfhksdjhfsdkjfhekyuiwefhkewjfkwjefiwehkfjhwekjfhewkj</div>
     <TestCom/>
     </div>
   );
